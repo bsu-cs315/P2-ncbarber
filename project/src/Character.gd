8fb587460e089ac7b0eb15738e0839bc95b8b550
Nathaniel <ncbarber11@gmail.com>
2021-09-27 20:58:13 -0400

    Finish Character Movement, and Start animations

--- project/src/Character.gd
+++ project/src/Character.gd
@@ -1,6 +1,37 @@
 extends KinematicBody2D
 
+var gravity = 500
+var jumpPower = -350
+var rocketJump = -500
+var speed = 200
+#var currentGravity = gravity
+var jumpped = false
+var velocity = Vector2()
 
-
-func _physics_process(delta) -> void:
+func _physics_process(delta):
+	velocity.x = 0
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jumpPower
+			jumpped = true
+	elif Input.is_action_pressed("rocket_launch"):
+		if is_on_floor():
+			velocity.y = rocketJump
+			jumpped = true
+	elif Input.is_action_pressed("right"):
+		velocity.x += speed
+		if is_on_floor():
+			$AnimatedSprite.animation = "walking"
+			$AnimatedSprite.flip_h = false
+			$AnimatedSprite.play()
+	elif Input.is_action_pressed("left"):
+		velocity.x -= speed
+		if is_on_floor():
+			$AnimatedSprite.animation = "walking"
+			$AnimatedSprite.flip_h = true
+			$AnimatedSprite.play()
 	
+	velocity.y += gravity * delta
+	if jumpped and is_on_floor():
+		jumpped = false
+	velocity = move_and_slide(velocity, Vector2(0,-1))
---
+++ project/src/Level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var character : KinematicBody2D
+
+func _ready():
+	make_player()
+
+func _process(_delta):
+	if Input.is_action_pressed("restart"):
+		restart_level()
+
+func make_player():
+	character = load("res://src/Character.tscn").instance()
+	character.position = Vector2(150,400)
+	call_deferred("add_child", character)
+
+func restart_level():
+	var _restart = get_tree().change_scene("res://src/Level.tscn")
