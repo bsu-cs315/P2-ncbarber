6ed3675d46d7920980f6d7f9a09b50fedc9289cd
Nathaniel <ncbarber11@gmail.com>
2021-09-27 21:19:40 -0400

    Finish Animations

--- project/src/Character.gd
+++ project/src/Character.gd
@@ -10,14 +10,19 @@ var velocity = Vector2()
 
 func _physics_process(delta):
 	velocity.x = 0
+
 	if Input.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jumpPower
 			jumpped = true
+			$AnimatedSprite.animation = "jumping"
+			$AnimatedSprite.play()
 	elif Input.is_action_pressed("rocket_launch"):
 		if is_on_floor():
 			velocity.y = rocketJump
 			jumpped = true
+			$AnimatedSprite.animation = "jumping"
+			$AnimatedSprite.play()
 	elif Input.is_action_pressed("right"):
 		velocity.x += speed
 		if is_on_floor():
@@ -30,6 +35,13 @@ func _physics_process(delta):
 			$AnimatedSprite.animation = "walking"
 			$AnimatedSprite.flip_h = true
 			$AnimatedSprite.play()
+	elif Input.is_action_pressed("down"):
+		if is_on_floor() and velocity.x == 0:
+			$AnimatedSprite.animation = "ducking"
+			$AnimatedSprite.play()
+	elif velocity.x == 0 and is_on_floor():
+		$AnimatedSprite.animation = "idle"
+		$AnimatedSprite.play()
 	
 	velocity.y += gravity * delta
 	if jumpped and is_on_floor():
