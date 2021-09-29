1e7cdef560f5695fc30856c0121a360fa501f063
Nathaniel <ncbarber11@gmail.com>
2021-09-29 16:33:10 -0400

    Fixed animations

--- project/src/Character.gd
+++ project/src/Character.gd
@@ -12,45 +12,53 @@ func _physics_process(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
 		velocity.x += speed
-		if is_on_floor():
-			$AnimatedSprite.animation = "walking"
-			$AnimatedSprite.play()
-			$AnimatedSprite.flip_h = false
 
 	if Input.is_action_pressed("left"):
 		velocity.x -= speed
-		if is_on_floor():
-			$AnimatedSprite.animation = "walking"
-			$AnimatedSprite.play()
-			$AnimatedSprite.flip_h = true
 
 	if Input.is_action_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jumpPower
 			jumpped = true
-			$AnimatedSprite.animation = "jumping"
-			$AnimatedSprite.play()
 
 	if Input.is_action_pressed("rocket_launch"):
 		if is_on_floor():
 			velocity.y = rocketJumpPower
 			rocketJumpped = true
-			$AnimatedSprite.animation = "rocket"
-			$AnimatedSprite.play()
-
-	if velocity.x == 0 and is_on_floor():
-		$AnimatedSprite.stop()
-		$AnimatedSprite.animation = "idle"
-		$AnimatedSprite.play()
-
-	if Input.is_action_pressed("down"):
-		if is_on_floor() and velocity.x == 0:
-			$AnimatedSprite.stop()
-			$AnimatedSprite.animation = "ducking"
-			$AnimatedSprite.play()
-		
-
+			
+	_process_animation()
 	velocity.y += gravity * delta
 	if jumpped and is_on_floor():
 		jumpped = false
+		
+	if rocketJumpped and is_on_floor():
+		rocketJumpped = false
 	velocity = move_and_slide(velocity, Vector2(0,-1))
+	
+	
+func _process_animation():
+	if Input.is_action_pressed("jump"):
+		$AnimatedSprite.animation = "jumping"
+		$AnimatedSprite.play()
+		
+	if Input.is_action_pressed("rocket_launch"):
+		$AnimatedSprite.animation = "rocket"
+		$AnimatedSprite.play()
+	
+	if Input.is_action_pressed("right") and is_on_floor():
+		$AnimatedSprite.animation = "walking"
+		$AnimatedSprite.play()
+		$AnimatedSprite.flip_h = false
+		
+	if Input.is_action_pressed("left") and is_on_floor():
+		$AnimatedSprite.animation = "walking"
+		$AnimatedSprite.play()
+		$AnimatedSprite.flip_h = true
+
+	if velocity.x == 0 and is_on_floor():
+		$AnimatedSprite.animation = "idle"
+		$AnimatedSprite.play()
+
+	if Input.is_action_pressed("down") and is_on_floor() and velocity.x == 0:
+		$AnimatedSprite.animation = "ducking"
+		$AnimatedSprite.play()
