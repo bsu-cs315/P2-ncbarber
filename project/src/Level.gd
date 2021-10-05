18a03a95ce5f6803dc741b8f76167b7d0132013f
Nathaniel <ncbarber11@gmail.com>
2021-10-04 22:58:11 -0400

    Finish coin variability

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var character : KinematicBody2D
-var HUD : Sprite
 var score := 5
 var timer := 0
 var running := true
@@ -16,16 +15,23 @@ func _ready():
 	if generatedNumber == 1:
 		spawn_coin(Vector2(-323, 217))
 		spawn_coin(Vector2(3040, -934))
+		spawn_coin(Vector2(-295, 225))
 		spawn_coin(Vector2(randomNumber.randi_range(1050, 2585), 465))
 	elif generatedNumber == 2:
 		spawn_coin(Vector2(0, -530))
 		spawn_coin(Vector2(2932, 715))
+		spawn_coin(Vector2(2144, -295))
+		spawn_coin(Vector2(randomNumber.randi_range(1050, 2585), 465))
 	elif generatedNumber == 3:
 		spawn_coin(Vector2(2614, 75))
+		spawn_coin(Vector2(75, -835))
+		spawn_coin(Vector2(1312, 144))
 		spawn_coin(Vector2(randomNumber.randi_range(378, 1425), -703))
 	elif generatedNumber == 4:
-		spawn_coin(Vector2(randomNumber.randi_range(2745, 3335), 15))
+		spawn_coin(Vector2(2525, -290))
 		spawn_coin(Vector2(1700, 90))
+		spawn_coin(Vector2(3313, -493))
+		spawn_coin(Vector2(randomNumber.randi_range(2745, 3335), 15))
 
 
 func _process(_delta):
